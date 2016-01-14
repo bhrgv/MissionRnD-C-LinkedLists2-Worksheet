a7e355d90e59b55ac400d5e66fd2ce26f875005d
bhrgv <bhrgv@missionrnd.com>
2016-01-14 11:43:47 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,5 +19,44 @@ struct node {
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	struct node dummy;
+	struct node* curr=&dummy;
+	struct node* curr1 = head1;
+	struct node* curr2 = head2;
+	if (head1 == NULL)
+		return head2;
+	else if (head2 == NULL)
+		return head1;
+	while (curr1 != NULL&&curr2 != NULL)
+	{
+		if (curr1->num < curr2->num)
+		{
+			curr->next = curr1;
+			curr1 = curr1->next;
+			curr = curr->next;
+			curr->next = NULL;
+		}
+		else
+		{
+			curr->next = curr2;
+			curr2 = curr2->next;
+			curr = curr->next;
+			curr->next = NULL;
+		}
+	}
+	while (curr1 != NULL)
+	{
+		curr->next = curr1;
+		curr1 = curr1->next;
+		curr = curr->next;
+		curr->next = NULL;
+	}
+	while (curr2 != NULL)
+	{
+		curr->next = curr2;
+		curr2 = curr2->next;
+		curr = curr->next;
+		curr->next = NULL;
+	}
+	return dummy.next;
 }
